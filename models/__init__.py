303ff73a69a15bde911d407097c6fdbb8def090c
CodigoByte2020 <gmcontrpuma@gmail.com>
2024-06-14 13:44:23 -0500

    [IMP] Firs commit

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from . import op_course
+from . import op_course_type
+from . import op_area_course
+from . import op_batch
+from . import op_campus
+from . import op_modality
+from . import op_practices_type
